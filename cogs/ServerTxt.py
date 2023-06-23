49986fa85e38dade13e0ec9b46ff60641791f836
Adis2020 <adiskd22gm@gmail.com>
2023-06-23 17:48:23 +0600

    changed: added and changed function for correct work

--- cogs/ServerTxt.py
+++ cogs/ServerTxt.py
@@ -1,26 +1,29 @@
 import json
+import os
 import discord
-from discord.ext import commands
-
-with open('data/server-txts/rule.txt', 'r', encoding='utf-8') as file:
-    rule = file.read()
+from discord.ext import commands, tasks
 
 with open('data/channelsId.json', 'r') as file:
     array_ch_id = json.load(file)
     channel_id_rule = array_ch_id['RuleChId']
 
 
-class sendServerTxts(commands.Cog):
+
+
+
+class ServerTxt(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.rule = rule
+        self.rule = None
         self.channelIdRule = int(channel_id_rule)
+        self.rule = open_file.start()
 
     @commands.command()
     async def sendRule(self, ctx):
         channel = self.bot.get_channel(self.channelIdRule)
+        await channel.purge()
         await channel.send(self.rule)
 
 
 async def setup(bot):
-    await bot.add_cog(sendServerTxts(bot))
+    await bot.add_cog(ServerTxt(bot))
